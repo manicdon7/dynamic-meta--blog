29c6e019afeff656c781c50057a7049365f508fa
manicdon7 <manicdon7@gmail.com>
2024-05-24 23:27:00 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"https://dynamic-meta-blog-client.vercel.app/"}));
 
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname,'..', 'build')));
